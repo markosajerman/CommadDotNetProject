b8be5af70d5bb97c85e12a0bcbac4f24e82be50d
Marko Sajerman <marko.sajerman1@gmail.com>
2024-04-17 15:23:30 +0200

    Added Model and Mock Repo

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,30 +1,34 @@
+using CommandAPI.Data;
+
 public class Startup
-{ 
-    public IConfiguration Configuration { get; }
-    public Startup(IConfiguration configuration)
-    {
-        this.Configuration = configuration;
-    }
+{
+	public IConfiguration Configuration { get; }
+	public Startup(IConfiguration configuration)
+	{
+		this.Configuration = configuration;
+	}
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddControllers();
-    }
-    // This method gets called by the runtime. Use this method to configure HTTP request pipeline
-    public void Configure(WebApplication app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
+	// This method gets called by the runtime. Use this method to add services to the container.
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddControllers();
+		// Dependency injection
+		services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+	}
+	// This method gets called by the runtime. Use this method to configure HTTP request pipeline
+	public void Configure(WebApplication app, IWebHostEnvironment env)
+	{
+		if (env.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
 
-        app.UseRouting();
+		app.UseRouting();
 
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-    }
+		app.UseEndpoints(endpoints =>
+		{
+			endpoints.MapControllers();
+		});
+	}
 }
 
